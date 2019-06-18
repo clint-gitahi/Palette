cf7d27d7c50cf0d9cca4b04abe5d23a6fc0be2cc
clintgitahi <clinton@yumkenya.com>
2019-06-18 12:56:32 +0300

    styling the color box

--- src/Palette.js
+++ src/Palette.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
+import "./Palette.css";
+import ColorBox from "./ColorBox";
 
 class Palette extends Component {
   render() {
+    const colorBoxes = this.props.colors.map(color => {
+      return <ColorBox background={color.color} name={color.name} />;
+    });
     return (
       <div className="Palette">
-        <div className="Palette-colors" />
+        <div className="Palette-colors">{colorBoxes}</div>
       </div>
     );
   }
