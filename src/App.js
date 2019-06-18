13a2b9c25a0870e5d4639c98bef30bb98cf5a86c
clint-gitahi <clingitahi@gmail.com>
2019-06-18 16:40:38 +0300

    helper function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
+import { generatePalette } from "./colorHelpers";
 
 class App extends Component {
   render() {
